f05ac08dc96215ee67ece12c698014eb5e78d5ad
funkee <xxtrm98@protonmail.com>
2023-03-02 20:08:07 -0800

    added component for test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Hero />
     </BrowserRouter>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <div className="bg-blizzard-100 w-full p-4 text-center justify-center">
+        <h1>Welcome</h1>
+        <p>Welcome to my Homepage</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
