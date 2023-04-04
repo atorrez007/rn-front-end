f0d1b6d61d52e04dbb6ea0fc7203f889019f7ada
funkee <xxtrm98@protonmail.com>
2023-04-04 08:49:01 -0700

    added count to front-end and  pagination template

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,20 +7,27 @@ import Navbar from "./Navbar";
 // Previously the <Link> was used to wrap all the hospitals in a link, but now the link is entered into the hospital card component. If we want to change accessibility to allow clicking anywhere on the card, we can revert to putting the link in the hospitalData.map once again.
 const Search = () => {
   const dispatch = useDispatch();
-  const allHospitalsState = useSelector(
-    (state) => state.hospitals.allHospitals
-  );
+  // const allHospitalsState = useSelector(
+  //   (state) => state.hospitals.allHospitals
+  // );
 
-  const hospitalState = useSelector((state) => state.hospitals.hospitals);
+  const hospitalCount = useSelector((state) => state.hospitals.hospitals.count);
+  // console.log(hospitalCount);
+  const hospitalState = useSelector((state) => state.hospitals.hospitals.data);
+  // console.log(hospitalState);
+  // console.log(hospitalState);
+
   const [rawData, setRawData] = useState([]);
-
   // allHospitals query to be used later
   const [fullList, setFullList] = useState([]);
+  const [page, setPage] = useState("");
+  const [state, setState] = useState("");
 
-  // const [hospitalData, setHospitalData] = useState([]);
-  const [page, setPage] = useState("1");
-  const [state, setState] = useState("AK");
-  // const [rawQuery, setRawQuery] = useState(false);
+  // const numOfPages = Math.ceil(12);
+
+  // const pageNumbers = [];
+  // for (let i = 0; i <= numOfPages; i++) {}
+
   const handleStateChange = (e) => {
     setState(e.target.value);
   };
@@ -31,9 +38,9 @@ const Search = () => {
     dispatch(getAllHospitals());
   }, [dispatch]);
 
-  useEffect(() => {
-    setFullList(allHospitalsState);
-  }, [allHospitalsState]);
+  // useEffect(() => {
+  //   setFullList(allHospitalsState);
+  // }, [allHospitalsState]);
 
   useEffect(() => {
     dispatch(getHospitals(url));
@@ -50,18 +57,19 @@ const Search = () => {
   useEffect(() => {
     fetch(`http://localhost:8000/hospitals?allHospitals=true`)
       .then((res) => res.json())
-      .then((data) => {
-        setRawData(data);
+      .then((hospitals) => {
+        setRawData(hospitals.data);
+        // console.log(hospitals.data);
         // dispatch(getHospitals(url)); // dispatching after getting all data
       });
   }, [dispatch, url]);
 
-  // const uniqueCities = rawData
-  //   .map((hospital) => hospital.city)
-  //   .filter((city, index, arr) => {
-  //     return arr.indexOf(city) === index;
-  //   })
-  //   .sort();
+  const uniqueCities = rawData
+    ?.map((hospital) => hospital.city)
+    .filter((city, index, arr) => {
+      return arr.indexOf(city) === index;
+    })
+    .sort();
 
   // const uniqueCities = [
   //   ...new Set(hospitalState.map((hospital) => hospital.city)),
@@ -75,6 +83,8 @@ const Search = () => {
   // const citiesFiltered = uniqueCities.map((city, index) => (
   //   <option key={index}>{city}</option>
   // ));
+
+  // Rely on Raw Data
 
   const uniqueStates = rawData
     .map((hospital) => hospital.state)
@@ -127,7 +137,7 @@ const Search = () => {
   //       })
   //   : null;
 
-  const hospitals = hospitalState.map((hospital) => (
+  const hospitals = hospitalState?.map((hospital) => (
     <HospitalCard
       key={hospital.hospitalId}
       name={hospital.name}
@@ -175,6 +185,93 @@ const Search = () => {
             {hospitals ? hospitals : null}
           </div>
         </div>
+        <nav aria-label="Page navigation example">
+          <ul className="inline-flex items-center -space-x-px">
+            <li>
+              <a
+                href="/"
+                className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                <span className="sr-only">Previous</span>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fillRule="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                1
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                2
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-current="page"
+                className="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+              >
+                3
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                4
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                5
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                <span className="sr-only">Next</span>
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5"
+                  fillRule="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
